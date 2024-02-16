6d1fbb7efd9b543dbaa61a9627511d4cc4761b67
ANDREI\neamt <neamtuxandrei@gmail.com>
2024-02-17 00:08:00 +0200

    add seed data and migration auto run at first build

---
+++ Backend/HomeControlAPI/HomeControlAPI.DataAccess/HomeControlSeedData.cs
@@ -0,0 +1,28 @@
+﻿using HomeControlAPI.Domain;
+
+namespace HomeControlAPI.DataAccess
+{
+    public class HomeControlSeedData
+    {
+        public static async Task SeedDataAsync(HomeControlDbContext context)
+        {
+            var temperatureSensorList = new List<TemperatureSensor>()
+            {
+                TemperatureSensor.Create(21.3M,Domain.Enums.TemperatureUnit.Celsius,"Sufragerie"),
+                TemperatureSensor.Create(13.93M,Domain.Enums.TemperatureUnit.Celsius,"Baie"),
+            };
+            var ledSensorList = new List<LEDSensor>()
+            {
+                LEDSensor.Create("Sufragerie"),
+                LEDSensor.Create("Sufragerie"),
+                LEDSensor.Create("Dormitor"),
+                LEDSensor.Create("Baie")
+            };
+
+            context.TemperatureSensors.AddRange(temperatureSensorList);
+            context.LEDSensors.AddRange(ledSensorList);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- Backend/HomeControlAPI/HomeControlAPI/Program.cs
+++ Backend/HomeControlAPI/HomeControlAPI/Program.cs
@@ -57,4 +57,31 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 
+
+bool isFirstRun = !File.Exists("firstrun.flag");
+if (isFirstRun)
+{
+    // to initiate migrations and seed data every time the app is built.
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var homeControlDbContext = services.GetRequiredService<HomeControlDbContext>();
+        var applicationDbContext = services.GetRequiredService<ApplicationDbContext>();
+        //RoleManager<IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        try
+        {
+            await homeControlDbContext.Database.MigrateAsync();
+            await applicationDbContext.Database.MigrateAsync();
+
+            await HomeControlSeedData.SeedDataAsync(homeControlDbContext);
+
+            File.WriteAllText("firstrun.flag", "Migration and seeding completed.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
 app.Run();
